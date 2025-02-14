b42a93eb69f823b900c3e85dedcede1815777e19
grahamsgithub <grahams1nbox@outlook.com>
2025-02-14 16:05:21 +1300

    update to M1LAB4

--- Module1-Lab-and-Class-Exercises/LabExercises/M1LAB4.js
+++ Module1-Lab-and-Class-Exercises/LabExercises/M1LAB4.js
@@ -1,3 +1,15 @@
+// in the below code we functions 
+// to add, subtract, multiply and divide two numbers. 
+// The functions are called with the numbers 5 and 10. 
+// The results are printed to the console. a and b are 
+// the parameters of the functions.
+// because we are only passing 2 numbers to the functions,
+// we can use a and b as the parameters.
+// otherwise the parameters should be more descriptive. for example,
+// function add(num1, num2) {
+//   return num1 + num2
+// }
+
 console.log('M1LAB4.js')
 console.log('---------')
 console.log('Things to do with the numbers 5 and 10:')
