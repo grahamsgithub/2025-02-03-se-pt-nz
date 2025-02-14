85ff397f9bb8e613ad4d3acacd0cb40e83a651d0
grahamsgithub <grahams1nbox@outlook.com>
2025-02-14 15:56:47 +1300

    Update: Local to Remote: Added Class and Lab work

---
+++ Module1-Lab-and-Class-Exercises/LabExercises/M1LAB4.js
@@ -0,0 +1,21 @@
+noconsole.log('M1LAB4.js')
+console.log('---------')
+console.log('Things to do with the numbers 5 and 10:')
+console.log('5 + 10 =', add(5, 10))
+console.log('5 - 10 =', subtract(5, 10))
+console.log('5 * 10 =', multiply(5, 10))
+console.log('5 / 10 =', devide(5, 10))
+
+function add(a, b) {
+  return a + b
+}   
+function subtract(a, b) {
+  return a - b
+}
+function multiply(a, b) {
+  return a * b
+}
+function devide(a, b) {
+  return a / b
+}
+
