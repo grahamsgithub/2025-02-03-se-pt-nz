2444547c45bf7a88e7d3efd40ab06794f3a7b8d7
grahamsgithub <grahams1nbox@outlook.com>
2025-04-30 17:57:04 +1200

    updates to base files for module 6

--- Module6/first-react-app/src/App.jsx
+++ Module6/first-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Comment from './Comment'
 import './App.css'
 
 function App() {
@@ -27,6 +28,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+        <Comment></Comment>
       </p>
     </>
   )
